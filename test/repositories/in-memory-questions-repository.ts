71ed7a39340ec348d1fb9194c919c79d87a9b224
Angelo Netho <angelonetho@gmail.com>
2024-04-01 19:44:23 -0300

    Caso de uso: Listar perguntas recentes

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,5 +1,7 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { a } from 'vitest/dist/suite-UrZdHRff'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   
@@ -24,6 +26,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     return question
   }
+  
+  async findManyRecent({ page }: PaginationParams) {
+      const questions = this.items.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+      return questions
+    }
 
   async create(question: Question) {
     this.items.push(question)
