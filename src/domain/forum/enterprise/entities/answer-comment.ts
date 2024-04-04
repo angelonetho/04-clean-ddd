2e06ff2b45526a1ba75891f1845ba820382b9dde
Angelo Netho <angelonetho@gmail.com>
2024-04-04 19:37:53 -0300

    Classe base de comentários

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,39 +1,16 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityId
+import { Comment, CommentProps } from './comment'
+
+export interface AnswerCommentProps extends CommentProps{
   answerId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
+export class AnswerComment extends Comment<AnswerCommentProps> {
 
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+  get answerId() {
+    return this.props.answerId
   }
 
   static create(
