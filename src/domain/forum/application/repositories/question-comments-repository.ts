bf4e4d4c1a58429e558d51a6a2cdf56ba5cad475
Angelo Netho <angelonetho@gmail.com>
2024-04-05 16:41:49 -0300

    Caso de uso: Comentar na pergunta

---
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -0,0 +1,7 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+
+export interface QuestionCommentsRepository {
+  create(questionComment: QuestionComment): Promise<void>
+
+}
